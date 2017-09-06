f29e3e5d62fb4042ac7ca18cd10af19f305dda27
Naoyuki Sogo <sogo@longtail-software.co.jp>
2017-09-07 00:18:00 +0900

    add beta version of home screen

---
+++ app/dst/public/js/home_beta.js
@@ -0,0 +1,70 @@
+$(function () {
+    // socket io
+    var socket = io('/home');
+    socket.on('projectList', function (data) {
+        if (!data.hasOwnProperty('label'))
+            return;
+        if (!data.hasOwnProperty('path'))
+            return;
+        $('#projectList').append("<li class=\"ui-state-default\"><span class=\"projectPath\">" + data.path + "</span>" + data.label + "</li>");
+    });
+    socket.on('files', function (data) {
+        if (!data.hasOwnProperty('name'))
+            return;
+        if (!data.hasOwnProperty('type'))
+            return;
+        if (!(data.type in ['file', 'dir', 'link']))
+            return;
+    });
+    //TODO 別のファイルへお引っ越し?
+    var openProject = function (key, opt) {
+        console.log('open project!');
+        //how to get project path from span element
+        console.log($(this).children('.projectPath').text());
+    };
+    var renameProject = function (key, opt) {
+        console.log('rename project!');
+    };
+    var deleteProject = function (key, opt) {
+        console.log('delete project!');
+    };
+    // register context Menu for projectList
+    $.contextMenu({
+        'selector': '#projectList li',
+        'items': {
+            'open': {
+                name: 'Open',
+                callback: openProject
+            },
+            'rename': {
+                name: 'Rename',
+                callback: renameProject
+            },
+            'delete': {
+                name: 'Delete',
+                callback: deleteProject
+            }
+        }
+    });
+    // setup project list UI
+    $('#projectList').sortable({
+        update: function (e, ui) {
+            socket.emit('reorder', $('#projectList').sortable('toArray'));
+        }
+    });
+    $('#projectList').disableSelection();
+    // btn click event
+    $('#btnNew').on("click", function (event) {
+        $('#normal').hide();
+        $('#dialogue').show();
+    });
+    $('#btnImport').on("click", function (event) {
+        $('#normal').hide();
+        $('#dialogue').show();
+    });
+    $('#btnCancel').on("click", function (event) {
+        $('#normal').show();
+        $('#dialogue').hide();
+    });
+});
+//# sourceMappingURL=home_beta.js.map
---
+++ app/src/public/js/home_beta.ts
@@ -0,0 +1,71 @@
+$(() => {
+    // socket io
+    const socket = io('/home');
+    socket.on('projectList', (data)=>{
+      if(!data.hasOwnProperty('label')) return; 
+      if(!data.hasOwnProperty('path')) return;
+      $('#projectList').append(`<li class="ui-state-default"><span class="projectPath">${data.path}</span>${data.label}</li>`);
+    });
+    socket.on('files', (data)=>{
+      if(!data.hasOwnProperty('name')) return;
+      if(!data.hasOwnProperty('type')) return;
+      if(!(data.type in ['file', 'dir', 'link'])) return;
+    });
+
+    //TODO 別のファイルへお引っ越し?
+    var openProject=function(key, opt){
+      console.log('open project!')
+      //how to get project path from span element
+      console.log($(this).children('.projectPath').text());
+    }
+    var renameProject=function(key, opt){
+      console.log('rename project!')
+    }
+    var deleteProject=function(key, opt){
+      console.log('delete project!')
+    }
+
+    // register context Menu for projectList
+    $.contextMenu({
+      'selector': '#projectList li',
+      'items': {
+        'open':{
+          name: 'Open',
+          callback: openProject
+        },
+        'rename':{
+          name: 'Rename',
+          callback: renameProject
+        },
+        'delete':{
+          name: 'Delete',
+          callback: deleteProject
+        }
+      }
+    });
+
+    // setup project list UI
+    $('#projectList').sortable({
+    update: (e,ui)=>{
+        socket.emit('reorder', $('#projectList').sortable('toArray'));
+        }
+    });
+    $('#projectList').disableSelection();
+
+
+    // btn click event
+    $('#btnNew').on("click", (event)=>{
+      $('#normal').hide();
+      $('#dialogue').show();
+    });
+    $('#btnImport').on("click", (event)=>{
+      $('#normal').hide();
+      $('#dialogue').show();
+    });
+    $('#btnCancel').on("click", (event)=>{
+      $('#normal').show();
+      $('#dialogue').hide();
+    });
+
+
+});
