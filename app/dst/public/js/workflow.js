a49efabeb7cb132a53dc14a81a5b33e0869c6257
Naoyuki Sogo <sogo@longtail-software.co.jp>
2017-09-22 13:34:15 +0900

    add show or hide log button

---
+++ app/dst/public/js/workflow.js
@@ -0,0 +1,18 @@
+$(() => {
+    var showLog = function () {
+        $('#log_area').show();
+    };
+    var hideLog = function () {
+        $('#log_area').hide();
+    };
+    $('#displayLog').button();
+    $('#displayLog').change(function () {
+        if ($('#displayLog').prop('checked')) {
+            showLog();
+        }
+        else {
+            hideLog();
+        }
+    });
+});
+//# sourceMappingURL=workflow.js.map
