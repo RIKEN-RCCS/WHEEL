4d730cbec831a502ae9b2f5957c5fcf491a09c9b
Naoyuki Sogo <sogo@longtail-software.co.jp>
2017-09-30 05:29:25 +0900

    remove debug output

--- app/server.js
+++ app/server.js
@@ -23,7 +23,6 @@ const sio = require('socket.io')(server);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-console.log(path.resolve('./app/public'));
 app.use(express.static(path.resolve('./app/public'), { index: false }));
 app.use(siofu.router);
 // routing
