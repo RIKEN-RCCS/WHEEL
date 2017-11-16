5af15be9295d194c58e71b3d9481c26f8963a85d
Naoyuki Sogo <sogo@longtail-software.co.jp>
2017-11-16 09:47:31 +0900

    refactor projectListManager with jsonArrayManager

--- app/jsonArrayManager.js
+++ app/jsonArrayManager.js
@@ -23,24 +23,41 @@ class JsonArrayManager{
   write(){
     return util.promisify(fs.writeFile)(this.filename, JSON.stringify(this.data, null, 4))
   }
+  /**
+   * add new entry
+   * @param {Object} - entry
+   * If the entry has 'id' key, the value will be overwritten.
+   */
   add(entry){
     entry.id=uuidv1();
     this.data.push(entry)
     return this.write();
   }
+  /**
+   * update entry
+   * @param {Object} - entry
+   */
   update(entry){
     let targetIndex=this.data.findIndex((e)=>{
       return e.id === entry.id;
     });
-    this.data[targetIndex] = entry;
+    Object.assign(this.data[targetIndex],entry);
     return this.write();
   }
+  /**
+   * remove existing entry
+   * @param {string} id - id of target entry
+   */
   remove(id){
     this.data=this.data.filter((e)=>{
       return e.id !== id;
     });
     return this.write();
   }
+  /**
+   * add duplicated entry
+   * @param {string} id - id of src entry
+   */
   copy(id){
     let target=this.data.find((e)=>{
       return e.id === id;
@@ -50,14 +67,40 @@ class JsonArrayManager{
     this.data.push(duplicate);
     return this.write();
   }
+  /**
+   * get the entry which have specified id
+   * @param {string} id - id
+   */
   get(id){
     return this.data.find((e)=>{
         if (e.id === id)
             return true;
     });
   }
+  /**
+   * getter for data array
+   */
   getAll(){
     return this.data;
+  }
+  /**
+   * reorder data array
+   * @param {string[]} newOrder - array of id's stored in new order
+   */
+  reorder(newOrder) {
+    if (this.data.length != newOrder.length) {
+      return;
+    }
+    const oldIndexList = newOrder.map((id)=>{
+      return this.data.findIndex((v,i)=>{
+        if(id === v.id) return true
+      });
+    });
+    var tmp = Array.from(this.data);
+    for(let i=0; i<tmp.length; i++){
+      this.data[i] = tmp[oldIndexList[i]];
+    }
+    return this.write();
   }
 }
 
--- app/projectListManager.js
+++ app/projectListManager.js
@@ -1,12 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const path = require("path");
 const util = require('util');
-const uuidv1 = require("uuid/v1");
+
 const logger = require("./logger");
 const config = require('./config/server');
-var projectList = [];
+const JsonArrayManager = require("./jsonArrayManager");
 /*
  *  projectList example
  *
@@ -19,100 +18,44 @@ var projectList = [];
  *  'id'   : 'xxxxxxxx-xxxxxxxxx-xxxxxxxxxxxxxxxx'
  * }]
  */
-var projectListFilename = path.resolve('./app',config.projectList) + '.json';
+const projectListFilename = path.resolve('./app',config.projectList) + '.json';
+let projectList = new JsonArrayManager(projectListFilename);
 
-fs.readFile(projectListFilename, function (err, data) {
-    if (err) {
-        logger.info(`project list file read failed. (${projectListFilename})`);
-        logger.info('using empty list as initial state');
-        return;
-    }
-    var fileData = JSON.parse(data.toString());
-    if (!Array.isArray(fileData)) {
-        logger.info(`project list file has illegal data structure. (${projectListFilename})`);
-        logger.info('using empty list as initial state');
-        return;
-    }
-    projectList = fileData;
-});
-var writing = false;
-var writeProjectListFile = function () {
-    if (writing) {
-        logger.debug('skip writing projectList at this time');
-        return;
-    }
-    writing = true;
-    fs.writeFile(projectListFilename, JSON.stringify(projectList, null, 4), function () {
-        writing = false;
-    });
-};
-/**
- * 条件に一致するプロジェクトを返す
- * @param query プロジェクトIDまたはpath
- */
-function getProject(query) {
-    return projectList.find(function (item) {
-        if (item.id === query || item.path === query)
-            return true;
-    });
-}
 function getAllProject() {
-  return Promise.all(projectList.map(function(v){
+  return Promise.all(projectList.getAll().map(function(v){
     return util.promisify(fs.readFile)(v.path)
             .then(function(projectJson){
-              var tmp = JSON.parse(projectJson);
+              let tmp = JSON.parse(projectJson);
               return Object.assign(tmp,v)
             });
   }));
 }
 function reorder(newOrder) {
-    if (projectList.length != newOrder.length) {
-        logger.warn(`illegal reorder array. original length: ${projectList.length} reorder array length: ${newOrder.length}`);
-    }
-    var oldIndexList = newOrder.map(function(id){
-      return projectList.findIndex(function(v,i){
-        if(id === v.id) return true
-      });
-    });
-    var tmp = Array.from(projectList);
-    for(let i=0; i<tmp.length; i++){
-      projectList[i] = tmp[oldIndexList[i]];
-    }
-    writeProjectListFile();
+  return projectList.reorder(newOrder)
+  .then(()=>{
     return this.getAllProject();
+  });
+}
+function getProject(id) {
+    return projectList.get(id)
 }
 function remove(id) {
-    var numProjects = projectList.length;
-    projectList = projectList.filter((item) => {
-        return (item.id != id);
-    });
-    if (projectList.length != numProjects) {
-        writeProjectListFile();
-    }
-    return this.getAllProject();
+  projectList.remove(id);
+  return this.getAllProject();
 }
 function rename(id, projectJsonFilePath) {
-    var index = projectList.findIndex((item) => {
-        if (item.id === id)
-            return true;
-    });
-    projectList[index].path = projectJsonFilePath;
-    writeProjectListFile();
-    return this.getAllProject();
+  let tmp = projectList.get(id);
+  tmp.path = projectJsonFilePath;
+  projectList.update(tmp);
+  return this.getAllProject();
 }
 /*
  * add new project Json file to project list
  * @param path  project Json file's path (must be absolute path but not checked for now)
  */
 function add(path) {
-    var exists = projectList.some(function (item) {
-        return item.path == path;
-    });
-    if (exists) return;
-    var uuid = uuidv1();
-    projectList.push({"path": path, "id": uuid });
-    writeProjectListFile();
-    return this.getAllProject();
+  projectList.add({"path": path});
+  return this.getAllProject();
 }
 exports.getProject = getProject;
 exports.getAllProject = getAllProject;
