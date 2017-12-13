f65dc639de8c20df3d2a76d1fec40f922e70815d
Naoyuki Sogo <sogo@longtail-software.co.jp>
2017-12-14 01:51:18 +0900

    add automatic open browser feature

--- app/wheel.js
+++ app/wheel.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
+const path = require('path');
+const opener = require('opener');
+const waitOn = require('wait-on');
+
+let {port} = require(path.join(__dirname, 'config/server'));
+//TODO impliment argument parser and suppress opening browser in multi user mode
+let url = `http://localhost:${port}`;
+let opt = {
+  resources: [url],
+  delay: 500,
+  timeout: 3000
+}
+waitOn(opt, (err)=>{
+  if(err){
+    console.log('fatal error occurred during waiting http server up',err);
+    return;
+  }
+  opener(url);
+});
 require('WHEEL')
