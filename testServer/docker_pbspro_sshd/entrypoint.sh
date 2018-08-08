6fc2db246a44a790223985cc5a68d87cd7d8a9a9
Naoyuki Sogo <sogo@longtail-software.co.jp>
2018-08-08 11:42:30 +0900

    add docker setting files to setup remote job server for testing

---
+++ testServer/docker_pbspro_sshd/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# modify PBS config file
+sed -i "s/PBS_SERVER=.*/PBS_SERVER=`hostname`/"     /etc/pbs.conf
+sed -i "s/PBS_START_MOM=0/PBS_START_MOM=1/"         /etc/pbs.conf
+sed -i "s/\$clienthost .*/\$clienthost `hostname`/" /var/spool/pbs/mom_priv/config
+
+# start PBS servers
+/etc/init.d/pbs start
+
+# modify PBS settings
+/opt/pbs/bin/qmgr -c "set server job_history_enable=True"
+/opt/pbs/bin/qmgr -c 'set queue workq max_queued="[o:PBS_ALL=2]"'
+
+
+#exec cmmand which is specified on docker run commandline
+exec "$@"
