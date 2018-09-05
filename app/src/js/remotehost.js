bbba86c79ed5ae8919d4c99bb5bb7ae11415d7b0
Tanaka Yuki <tanaka.yuk@jp.fujitsu.com>
2018-09-05 13:05:35 +0900

    modify remotehost action.

--- app/src/js/remotehost.js
+++ app/src/js/remotehost.js
@@ -26,8 +26,7 @@ $(() => {
     jobScheduler: '',
     renewInterval: 0,
     renewDelay: 0,
-    execInterval: 1,
-    statusCheckInterval: 5,
+    statusCheckInterval: 0,
     maxStatusCheckError: 10,
   }
 
@@ -50,7 +49,9 @@ $(() => {
         if (this.selectedHost === i) {
           this.selectedHost = -1;
           resetNewHost();
+          notFormColor();
         } else {
+          formColor();
           this.selectedHost = i;
           Object.assign(this.newHostInfo, this.hostList[this.selectedHost]);
           vm.authType = (!this.newHostInfo.keyFile || this.newHostInfo.keyFile.length === 0) ? '1' : '2';
@@ -67,7 +68,7 @@ $(() => {
       onAddButton: function () {
         this.selectedHost = -1
         resetNewHost();
-        this.errorMessage = '';
+        // this.errorMessage = '';
         $("#errorMessage").css("visibility", "hidden");
       },
       onCopyButton: function () {
@@ -151,7 +152,7 @@ $(() => {
           name: !isEmpty(this.newHostInfo.name),
           host: !isEmpty(this.newHostInfo.host),
           username: !isEmpty(this.newHostInfo.username),
-          //path: !isEmpty(this.newHostInfo.path)
+          path: !isEmpty(this.newHostInfo.path)
         }
       },
       hasError: function () {
@@ -178,7 +179,8 @@ $(() => {
     const fb = new FileBrowser(socket, '#fileList', 'fileList');
     dialogWrapper('#dialog', html, dialogOptions)
       .done(function () {
-        let target = fb.getRequestedPath() + '/' + fb.getLastClicked()
+        let target = fb.getRequestedPath() + '/' + fb.getLastClicked();
+        console.log(target);
         socket.emit('add', target);
         vm.hostinfo.keyFile = target;
       });
@@ -234,6 +236,52 @@ $(() => {
     return string === '';
   }
 
+  // 追加部分　編集時に黄色い枠にする
+  function formColor() {
+    $('#hostRegFormArea input:not([type="radio"])').css('border', '1px solid #ccff00');
+  }
+  function notFormColor() {
+    $('#hostRegFormArea input:not([type="radio"])').css('border', '1px solid #000000');
+  }
+  $("#newButton").click(formColor);
+  $("#copyButton").click(notFormColor);
+  $("#deleteButton").click(notFormColor);
+  $("#cancelButton").click(notFormColor);
+  $("#confirmButton").click(notFormColor);
+
+
   var pos = $("#titleUserName").offset();
-  $("#img_user").css('right', window.innerWidth - pos.left + "px");
+  $("#iconImg").css('right', window.innerWidth - pos.left + "px");
+
+
+
+  // スクロールについて
+  function $E(name) { return document.getElementById(name); }
+  function scroll() {
+    $E("hostTitleArea").scrollLeft = $E("hostPropertyArea").scrollLeft;// 左右連動させる
+  }
+  $E("hostPropertyArea").onscroll = scroll;
+
+  //   /** スクロールバーの幅 (ピクセル単位) を取得する */
+  // function getScrollbarWidth() {
+  //   // スクロールバーの幅を取得するための要素を生成する   
+  //   const scrollbarElem = document.createElement('div');
+  //   // 要素の幅をビューポート幅 (スクロールバーを含む幅) で指定する・要素が画面に表示されないよう処理しておく
+  //   scrollbarElem.setAttribute('style', 'visibility: hidden; position: absolute; top: 0; left: 0; width: 100vw;');
+  //   // 画面表示上のピクセル数を拾うために一旦 body 要素に挿入する
+  //   document.body.appendChild(scrollbarElem);
+  //   // ビューポート幅を取得しておく ('0px' と単位付きで取得されるので parseInt() で単位を除去する)
+  //   const vw = parseInt(window.getComputedStyle(scrollbarElem).width);
+  //   // 要素の幅をパーセント幅 (スクロールバーを除く幅) で指定する
+  //   scrollbarElem.style.width = '100%';
+  //   // パーセント幅を取得する
+  //   const pc = parseInt(window.getComputedStyle(scrollbarElem).width);
+  //   // 要素を削除する
+  //   document.body.removeChild(scrollbarElem);
+  //   // ビューポート幅とパーセント幅の差分がスクロールバーの幅 (px) となる
+  //   const scrollbarWidth = vw - pc;
+  //   // スクロールバーの幅を返却する
+  //   return scrollbarWidth;
+  // }
+  // console(scrollbarWidth);
 });
