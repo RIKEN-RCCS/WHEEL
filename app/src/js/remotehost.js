576cbf444da6baa0f8747a9d274563d8d0ef66ec
Tanaka Yuki <tanaka.yuk@jp.fujitsu.com>
2018-10-25 14:53:59 +0900

    fix #241

--- app/src/js/remotehost.js
+++ app/src/js/remotehost.js
@@ -68,7 +68,6 @@ $(() => {
       onAddButton: function () {
         this.selectedHost = -1
         resetNewHost();
-        // this.errorMessage = '';
         $("#errorMessage").css("visibility", "hidden");
       },
       onCopyButton: function () {
@@ -179,10 +178,10 @@ $(() => {
     const fb = new FileBrowser(socket, '#fileList', 'fileList');
     dialogWrapper('#dialog', html, dialogOptions)
       .done(function () {
-        let target = fb.getRequestedPath() + '/' + fb.getLastClicked();
+        let target = fb.getRequestedPath() + '\\' + fb.getLastClicked();
         console.log(target);
         socket.emit('add', target);
-        vm.hostinfo.keyFile = target;
+        vm.newHostInfo.keyFile = target;
       });
     $('#fileList').empty();
     fb.resetOnClickEvents();
@@ -249,39 +248,15 @@ $(() => {
   $("#cancelButton").click(notFormColor);
   $("#confirmButton").click(notFormColor);
 
-
   var pos = $("#titleUserName").offset();
   $("#iconImg").css('right', window.innerWidth - pos.left + "px");
 
-
-
   // スクロールについて
-  function $E(name) { return document.getElementById(name); }
+  function $E(name) {
+    return document.getElementById(name);
+  }
   function scroll() {
     $E("hostTitleArea").scrollLeft = $E("hostPropertyArea").scrollLeft;// 左右連動させる
   }
   $E("hostPropertyArea").onscroll = scroll;
-
-  //   /** スクロールバーの幅 (ピクセル単位) を取得する */
-  // function getScrollbarWidth() {
-  //   // スクロールバーの幅を取得するための要素を生成する   
-  //   const scrollbarElem = document.createElement('div');
-  //   // 要素の幅をビューポート幅 (スクロールバーを含む幅) で指定する・要素が画面に表示されないよう処理しておく
-  //   scrollbarElem.setAttribute('style', 'visibility: hidden; position: absolute; top: 0; left: 0; width: 100vw;');
-  //   // 画面表示上のピクセル数を拾うために一旦 body 要素に挿入する
-  //   document.body.appendChild(scrollbarElem);
-  //   // ビューポート幅を取得しておく ('0px' と単位付きで取得されるので parseInt() で単位を除去する)
-  //   const vw = parseInt(window.getComputedStyle(scrollbarElem).width);
-  //   // 要素の幅をパーセント幅 (スクロールバーを除く幅) で指定する
-  //   scrollbarElem.style.width = '100%';
-  //   // パーセント幅を取得する
-  //   const pc = parseInt(window.getComputedStyle(scrollbarElem).width);
-  //   // 要素を削除する
-  //   document.body.removeChild(scrollbarElem);
-  //   // ビューポート幅とパーセント幅の差分がスクロールバーの幅 (px) となる
-  //   const scrollbarWidth = vw - pc;
-  //   // スクロールバーの幅を返却する
-  //   return scrollbarWidth;
-  // }
-  // console(scrollbarWidth);
 });
