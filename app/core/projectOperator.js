457f3aed72ffb050284008468e40875be347c7d7
Naoyuki Sogo <sogo@longtail-software.co.jp>
2019-02-13 22:34:57 +0900

    remove projectOperator separate getHost functionality from validation check

--- app/core/projectOperator.js
+++
@@ -1,8 +0,0 @@
-async function runProject(projectRootDir){
-  const projectJson = await readJsonGreedy(path.resolve(projectRootDir, projectJsonFilename));
-  const rootWF = await getComponent(projectRootDir, path.join(projectRootDir, componentJsonFilename));
-    
-}
-module.exports={
-  runProject
-}
