5a8bb8c5d303e1a05cbb48a8eb03c165cdacd1e1
Naoyuki Sogo <sogo@longtail-software.co.jp>
2019-07-24 11:53:09 +0900

    fix 402

--- app/routes/fileManager.js
+++ app/routes/fileManager.js
@@ -29,7 +29,7 @@ async function getComponentDirs(target) {
 async function sendDirectoryContents(emit, target, request, withSND = true, sendDir = true, sendFile = true, allFilter = /.*/) {
   request = request || target;
   const componentDirs = await getComponentDirs(request);
-  const regexComponentDirs = new RegExp(`(${componentDirs.join("|")})`);
+  const regexComponentDirs = new RegExp(`^(?!.*(${componentDirs.join("|")})).+$`);
   const result = await fileBrowser(target, {
     request,
     SND: withSND,
